1f9e21adc2b698406b09ef1bb433c5c928bbd716
Kirtan Bhatt <kirtan_bhatt01@infosys.com>
2023-03-03 15:34:38 +0100

    First commit

---
+++ src/main/java/com/mms/greetings/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.mms.greetings;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class HelloWorldController {
+    
+    @Autowired
+    private Environment environment;
+
+    private String greetingMessage = "Guten Tag. Wilkommen bei Infosys!!";
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String HelloWorld(){
+        greetingMessage = environment.getProperty("mms.greeting.message");
+        return greetingMessage.isEmpty() ? "Guten Tag. Wilkommen bei Infosys!!" : greetingMessage;
+    }
+}
