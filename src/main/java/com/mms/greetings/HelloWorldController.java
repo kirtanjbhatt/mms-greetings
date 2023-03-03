b5d5bbe37237490eff64951ad5d2b81198459cf2
Kirtan Bhatt <kirtan_bhatt01@infosys.com>
2023-03-03 19:53:48 +0100

    code fix

--- src/main/java/com/mms/greetings/HelloWorldController.java
+++ src/main/java/com/mms/greetings/HelloWorldController.java
@@ -1,24 +1,18 @@
 package com.mms.greetings;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class HelloWorldController {
-    
-    @Autowired
-    private Environment environment;
 
-    private String greetingMessage = "Guten Tag. Wilkommen bei Infosys!!";
-
-    private String propertyKey = "mms.greeting.message";
+    @Value("${mms.greeting.message}")
+    private String greetingMessage;
 
     @RequestMapping("/")
     @ResponseBody
     public String HelloWorld(){
-        greetingMessage = environment.containsProperty(propertyKey) ? environment.getProperty(propertyKey) : "";
-        return greetingMessage.isEmpty() ? "Guten Tag. Wilkommen bei Infosys!!" : greetingMessage;
+        return greetingMessage;
     }
 }
