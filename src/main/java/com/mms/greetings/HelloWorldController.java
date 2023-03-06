fa9ee4c4c4bf7a4ace637c50b2f8a60282348f97
Kirtan Bhatt <kirtan_bhatt01@infosys.com>
2023-03-06 11:37:14 +0100

    encryption test

--- src/main/java/com/mms/greetings/HelloWorldController.java
+++ src/main/java/com/mms/greetings/HelloWorldController.java
@@ -1,6 +1,6 @@
 package com.mms.greetings;
 
-import java.util.Base64;
+//import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -15,8 +15,8 @@ public class HelloWorldController {
     @RequestMapping("/")
     @ResponseBody
     public String HelloWorld(){
-        byte[] decodedBytes = Base64.getDecoder().decode(greetingMessage);
-        String decodedGreeetingsMessage = new String(decodedBytes);
-        return "Hello!!" + decodedGreeetingsMessage;
+        //byte[] decodedBytes = Base64.getDecoder().decode(greetingMessage);
+        //String decodedGreeetingsMessage = new String(decodedBytes);
+        return greetingMessage;
     }
 }
