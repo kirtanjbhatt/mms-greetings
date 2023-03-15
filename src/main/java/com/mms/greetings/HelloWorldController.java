0d40ff3faf8190d2d5582b4150e08aaf22d2f440
Kirtan Bhatt <kirtan_bhatt01@infosys.com>
2023-03-15 06:41:24 +0100

    Repo config

--- src/main/java/com/mms/greetings/HelloWorldController.java
+++ src/main/java/com/mms/greetings/HelloWorldController.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 
 @Controller
 public class HelloWorldController {
@@ -17,10 +20,28 @@ public class HelloWorldController {
     @Value("${mms.greeting.message.location}")
     private String messagePath;
 
+    @Value("${mms.repo.greeting.message}")
+    private String messageFromRepo;
+
+    @Value("${user.role}")
+    private String role;
+
+
     @RequestMapping("/")
     @ResponseBody
     public String HelloWorld(){        
         return greetingMessage;
+    }
+
+    @RequestMapping("/greetfromrepo")
+    @ResponseBody
+    public String HelloWorldFromRepo(){        
+        return messageFromRepo;
+    }
+
+    @GetMapping(value = "/whoami/{username}")
+    public String whoami(@PathVariable("username") String username) {
+        return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
     }
 
     @RequestMapping("/greet")
